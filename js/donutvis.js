f53e419a7c37a0ef394c0b9593b8a1e51e58852d
Anshika <anshikat21@vt.edu>
2022-04-01 21:00:53 -0400

    donut chart try

---
+++ js/donutvis.js
@@ -0,0 +1,115 @@
+
+/*
+ * AgeVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the actual data
+ */
+
+class DonutVis {
+
+    constructor(_parentElement, _data) {
+        this.parentElement = _parentElement;
+        this.data = _data;
+        this.initVis();
+
+    }
+
+
+    /*
+     * Initialize visualization (static content, e.g. SVG area or axes)
+     */
+
+    initVis() {
+        let vis = this;
+        console.log("here");
+        vis.margin = { top: 20, right: 20, bottom: 200, left: 60 };
+
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right,
+            vis.height = 500 - vis.margin.top - vis.margin.bottom;
+
+        // SVG drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+        vis.radius = Math.min(vis.width, vis.height) / 2 - vis.margin.right-vis.margin.left;
+
+        // vis.data = {a: 9, b: 20, c:30, d:8, e:12};
+        console.log(vis.data);
+        vis.color = d3.scaleOrdinal()
+            .domain(vis.data.map(d=>d.job_title_sim))
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56","#98abc5", "#8a89a6", "#7b6888"]);
+
+        vis.pie = d3.pie()
+            .value(function(d) {return d.value; });
+        // vis.data_ready = vis.pie(d3.entries(vis.data));
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+        vis.svg
+            .selectAll('whatever')
+            .data(vis.data,d=>d.job_title_sim)
+            .enter()
+            .append('path')
+            .attr('d', function(d) {
+                console.log(d);
+                return d3.arc()
+                    .innerRadius(100)         // This is the size of the donut hole
+                    .outerRadius(200);
+            })
+            .attr('fill', function(d){ return(vis.color(d.job_title_sim)) })
+            .attr("stroke", "black")
+            .style("stroke-width", "2px")
+            .style("opacity", 0.7);
+
+
+
+
+        // Scales and axes
+        // vis.x = d3.scaleLinear()
+        //     .range([0, vis.width])
+        //     .domain([0, 99]);
+        //
+        // vis.y = d3.scaleLinear()
+        //     .range([vis.height, 0]);
+        //
+        // vis.xAxis = d3.axisBottom()
+        //     .scale(vis.x);
+        //
+        // vis.yAxis = d3.axisLeft()
+        //     .scale(vis.y);
+
+
+
+        // (Filter, aggregate, modify data)
+        vis.wrangleData();
+    }
+
+
+
+    /*
+     * Data wrangling
+     */
+
+    wrangleData() {
+        let vis = this;
+
+        // Here you want to aggregate the data by age, not by day (as it is given)!
+
+
+        vis.updateVis();
+    }
+
+
+
+    /*
+     * The drawing function
+     */
+
+    updateVis() {
+        let vis = this;
+
+    }
+
+
+}
--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
+loadData();
+
 //reading csv file
-d3.csv("data/Market_Divers.csv").then(csv => {
-    csv.forEach(function (d) {
-        d.index = +d.index;
-        d.Rating = +d.Rating;
-        d.Founded = +d.Founded;
-        d.Lower_Salary = +d.Lower_Salary;
-        d.Upper_Salary = +d.Upper_Salary;
-        d.Avg_Salary = +d.Avg_Salary;
-        d.Python = +d.Python;
-        d.spark = +d.spark;
-        d.aws = +d.aws;
-        d.excel = +d.excel;
-        d.sql = +d.sql;
-        d.sas = +d.sas;
-        d.keras = +d.keras;
-        d.pytorch = +d.pytorch;
-        d.scikit = +d.scikit;
-        d.tensor = +d.tensor;
-        d.hadoop = +d.hadoop;
-        d.tableau = +d.tableau;
-        d.bi = +d.bi;
-        d.flink = +d.flink;
-        d.mongo = +d.mongo;
-        d.google_an = +d.google_an;
+function loadData() {
+    d3.csv("data/Market_Divers.csv").then(csv => {
+        csv.forEach(function (d) {
+            d.index = +d.index;
+            d.Rating = +d.Rating;
+            d.Founded = +d.Founded;
+            d.Lower_Salary = +d.Lower_Salary;
+            d.Upper_Salary = +d.Upper_Salary;
+            d.Avg_Salary = +d.Avg_Salary;
+            d.Python = +d.Python;
+            d.spark = +d.spark;
+            d.aws = +d.aws;
+            d.excel = +d.excel;
+            d.sql = +d.sql;
+            d.sas = +d.sas;
+            d.keras = +d.keras;
+            d.pytorch = +d.pytorch;
+            d.scikit = +d.scikit;
+            d.tensor = +d.tensor;
+            d.hadoop = +d.hadoop;
+            d.tableau = +d.tableau;
+            d.bi = +d.bi;
+            d.flink = +d.flink;
+            d.mongo = +d.mongo;
+            d.google_an = +d.google_an;
+        });
+        // console.log(csv);
+        let donutVis=new DonutVis("donutvis",csv);
     });
-    console.log(csv);
-});
+};
+// console.log(data);
+// function createVis(){
+//     let data = loadData();
+//     console.log(data);
+//
+// };
