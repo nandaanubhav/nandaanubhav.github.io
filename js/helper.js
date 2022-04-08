e363c50853a3cac80d48ca4c1f6713899f4506f5
Anshika <anshikat21@vt.edu>
2022-04-08 10:44:30 -0400

    merging Anshika's visuals

--- js/helper.js
+++ js/helper.js
@@ -6,24 +6,24 @@ $header_top.find('a').on('click', function () {
 });
 
 $('#fullpage').fullpage({
-    sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
+    sectionsColor: ['#83adb5', '#EBC1A2', '#c7bbc9', '#bfb5b2', '#ffd03c', '#f2e090', '#348899', '#ff8b20'],
     sectionSelector: '.vertical-scrolling',
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection'],
     menu: '#menu',
 
     afterLoad: function (anchorLink, index) {
         $header_top.css('background', 'rgba(0, 47, 77, .3)');
         $nav.css('background', 'rgba(0, 47, 77, .25)');
-        if (index == 5) {
+        if (index == 8) {
             $('#fp-nav').hide();
         }
     },
 
     onLeave: function (index, nextIndex, direction) {
-        if (index == 5) {
+        if (index == 8) {
             $('#fp-nav').show();
         }
     },
