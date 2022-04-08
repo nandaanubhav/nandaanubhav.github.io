447205e713928583d2626152b1f9e11d4fbdd776
Anshika <anshikat21@vt.edu>
2022-04-08 10:19:32 -0400

    final data+submission prototype 2

--- js/parallelvis.js
+++ js/parallelvis.js
@@ -21,14 +21,13 @@ d3.csv("data/Market_Divers.csv").then(data => {
 
 
     // console.log(data);
-    // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
-    // dimensions = Object.keys(data).filter(function(d) { return d != "Species" });
 
-    dimensions=["Calculated Size","Founded","Calculated_Revenue","Avg_Salary"]
+
+    dimensions=["Calculated_Size","Founded","Calculated_Revenue","Avg_Salary"]
     console.log(dimensions);
     // For each dimension, I build a linear scale. I store all in a y object
     var y = {}
-    y["Calculated Size"]=d3.scalePoint().domain(data.map(function(d){if(d["Calculated Size"]=="")return "Small"; return d["Calculated Size"]})).rangeRound([height,0]);
+    y["Calculated_Size"]=d3.scalePoint().domain(data.map(d=>d["Calculated_Size"])).rangeRound([height,0]);
 
     // console.log(y);
     y["Calculated_Revenue"]=d3.scalePoint().domain(data.map(d=>d["Calculated_Revenue"])).rangeRound([height,0]);
@@ -45,7 +44,7 @@ d3.csv("data/Market_Divers.csv").then(data => {
 
     var color = d3.scaleOrdinal()
         .domain(["Small", "Medium", "Large","Enterprise" ])
-        .range([ "grey","grey","grey","#440154ff"])
+        .range(["grey","grey","grey","black"])
 
     // Build the X scale -> it find the best position for each Y axis
     x = d3.scalePoint()
@@ -53,42 +52,10 @@ d3.csv("data/Market_Divers.csv").then(data => {
         .padding(1)
         .domain(dimensions);
 
-    // var color = d3.scaleOrdinal()
-    //     .domain(["setosa", "versicolor", "virginica" ])
-    //     .range([ "#440154ff", "#21908dff", "#fde725ff"]);
-    //
-    // // Highlight the specie that is hovered
-    // var highlight = function(d){
-    //
-    //     console.log(d);
-    //     selected_specie = d.Species;
-    //     console.log(selected_specie);
-    //
-    //     // first every group turns grey
-    //     d3.selectAll(".line")
-    //         .transition().duration(200)
-    //         .style("stroke", "lightgrey")
-    //         .style("opacity", "0.2")
-    //     // Second the hovered specie takes its color
-    //     d3.selectAll("." + selected_specie)
-    //         .transition().duration(200)
-    //         .style("stroke", color(selected_specie))
-    //         .style("opacity", "1")
-    // }
-    //
-    // // Unhighlight
-    // var doNotHighlight = function(d){
-    //     d3.selectAll(".line")
-    //         .transition().duration(200).delay(1000)
-    //         .style("stroke", function(d){ return( color(d.Species))} )
-    //         .style("opacity", "1")
-    // }
 
     // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
     function path(d) {
         return d3.line()(dimensions.map(function (p) {
-            // console.log(d);
-            // console.log(p + ' ' + len + ' ' + d + ' ' + y[len](d));
             return [x(p), y[p](d[p])];
         }));
     };
@@ -100,27 +67,19 @@ d3.csv("data/Market_Divers.csv").then(data => {
         .enter().append("path")
         .attr("d", path)
         .style("fill", "none")
-        // .style("stroke", "#69b3a2")
-        .style("stroke", function(d){ return( color(d["Calculated Size"]))} )
+        .style("stroke", "blue")
+        // .style("stroke", function(d){ return( color(d["Calculated_Size"]))} )
         .style("opacity", 0.5);
-    // .attr("class", function (d) { return "line " + d.Species } ) // 2 class for each line: 'line' and the group name
-    // .attr("d",  path)
-    // .style("fill", "none" )
-    // .style("stroke", function(d){ return( color(d.Species))} )
-    // .style("opacity", 0.5)
-    // .on("mouseover", highlight)
-    // .on("mouseleave", doNotHighlight );
+
 
     // Draw the axis:
     svg.selectAll("myAxis")
-        // For each dimension of the dataset I add a 'g' element:
         .data(dimensions).enter()
         .append("g")
-        // I translate this element to its right position on the x axis
         .attr("transform", function (d) {
             return "translate(" + x(d) + ")";
         })
-        // And I build the axis with the call function
+
         .each(function (d) {
             d3.select(this).call(d3.axisLeft().scale(y[d]));
         })
