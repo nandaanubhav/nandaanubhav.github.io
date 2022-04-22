52c0e3c7e22f53471089d81a50f6121884b272d0
Anshika <anshikat21@vt.edu>
2022-04-22 09:10:33 -0400

    parallel vis

--- js/parallelvis.js
+++ js/parallelvis.js
@@ -51,138 +51,139 @@ class ParallelVis {
         vis.y["Avg_Salary"] = d3.scaleLinear().domain(d3.extent(vis.data, function (d) {
             return +d["Avg_Salary"];
         })).range([vis.height, 0]);
-
-        vis.brush=function (event,d) {
-
-            var actives = [];
+        var extents=[];
+        vis.brush = function (event) {
             //filter brushed extents
-            vis.svg.selectAll(".brush")
-                .filter(function (d) {
-                    return d3.brushSelection(this);
-                })
-                .each(function (d) {
-                    actives.push({
-                        dimension: d,
-                        extent: d3.brushSelection(this)
-                    });
-                });
-            if (!event.selection && actives.length > 0) {
-                console.log("inside");
-                actives = actives.filter(active => active.dimension != d)
+
+            if (event.path) {
+
+                extents[vis.dimensions.indexOf(event.path[1].__data__)] = [0, 0];
             }
 
-
-            // console.log(csv);
-            var selected = vis.data.filter(function (d) {
-                if (actives.every(function (active) {
-                    var dim = active.dimension;
-                    // test if point is within extents for each active brush
-                    return active.extent[0] <= vis.y[dim](d[dim]) && vis.y[dim](d[dim]) <= active.extent[1];
-                })) {
-                    return true;
+            for (var i = 0; i < vis.dimensions.length; ++i) {
+                if (event.target == vis.y[vis.dimensions[i]].brush) {
+                    if (i == 0 || i == 2) {
+                        var selected = vis.y[vis.dimensions[i]].domain().filter(function (d) {
+                            var s = event.selection;
+                            return (s[0] <= vis.y[vis.dimensions[i]](d)) && (vis.y[vis.dimensions[i]](d) <= s[1])
+                        });
+                        var temp = selected.sort();
+                        extents[i] = [temp[temp.length - 1], temp[0]];
+                    } else {
+                        extents[i] = event.selection.map(vis.y[vis.dimensions[i]].invert, vis.y[vis.dimensions[i]]);
+                    }
                 }
+            }
+
+            var selected = vis.data.filter(function (d) {
+                return vis.dimensions.every(function (p, i) {
+                    if (!extents[i] || (extents[i][0] == 0 && extents[i][1] == 0)) {
+                        return true;
+                    }
+                    return extents[i][1] <= d[p] && d[p] <= extents[i][0];
+                }) ? true : false;
             });
-            console.log(selected);
+            // console.log(selected);
             if (selected.length != 0)
                 document.getElementById("salary").innerHTML = "Average Salary of selection :" + selected.reduce((r, c) => r + c.Avg_Salary, 0) / selected.length;
             else
                 document.getElementById("salary").innerHTML = "  ";
 
-            //set un-brushed foreground line disappear
-            vis.foreground.classed("fade", function (d, i) {
 
-                return !actives.every(function (active) {
-                    var dim = active.dimension;
-                    // console.log(y[dim](d[dim]));
-                    return active.extent[0] <= vis.y[dim](d[dim]) && vis.y[dim](d[dim]) <= active.extent[1];
+            // console.log(extents);
+            vis.foreground.style("display", function (d) {
+                return vis.dimensions.every(function (p, i) {
+                    if (!extents[i] || (extents[i][0] == 0 && extents[i][1] == 0)) {
+                        return true;
+                    }
+                    return extents[i][1] <= d[p] && d[p] <= extents[i][0];
+                }) ? null : "none";
+            });
+        }
+            vis.dimensions.forEach(d => {
+                vis.y[d].brush = d3.brushY()
+                    .extent([[-8, vis.y[d].range()[1]], [8, vis.y[d].range()[0]]])
+                    .on('brush', vis.brush);
+            });
+
+            vis.x.domain(vis.dimensions);
+            // Extract the list of dimensions and create a scale for each.
+
+
+            vis.updateVis();
+
+        }
+
+
+        updateVis()
+        {
+            let vis = this;
+            // Add grey background lines for context.
+            vis.background = vis.svg.append("g")
+                .attr("class", "background")
+                .selectAll("path")
+                .data(vis.data)
+                .enter().append("path")
+                .attr("d", path);
+
+            // Add blue foreground lines for focus.
+            vis.foreground = vis.svg.append("g")
+                .attr("class", "foreground")
+                .selectAll("path")
+                .data(vis.data)
+                .enter().append("path")
+                .attr("d", path);
+
+            //// ==== Draw Axis START ==== ////
+            vis.g = vis.svg.selectAll(".dimension")
+                .data(vis.dimensions).enter() // g's data is the dimensions
+                .append("g")
+                .attr("class", "dimension")
+                // Give the axis it's propper x pos
+                .attr("transform", function (d) {
+                    return "translate(" + vis.x(d) + ")";
                 });
-            });
+            vis.g.append("g") // The Axis
+                .attr("class", "axis")
+                .each(function (d) {
+                    d3.select(this).call(vis.axis.scale(vis.y[d]));
+                });
+            vis.g.append("text") // Axis Label
+                .attr("class", "title")
+                .style("text-anchor", "middle")
+                .attr("y", -10)
+                .attr("font-size", 15)
+                .text(function (d) {
+                    return d;
+                });
+            //// ==== Draw Axis END ==== ////
 
+            vis.g.append('g')
+                .attr('class', 'brush')
+                .each(function (d) {
+                    d3.select(this).call(vis.y[d].brush);
+                })
+                .selectAll('rect')
+                .attr('x', -8)
+                .attr('width', 16);
+
+            vis.svg.selectAll(".brush").selectAll(".overlay")
+                .on("mousedown", vis.brush);
+
+
+            function position(d) {
+                var v = vis.dragging[d];
+                return v == null ? vis.x(d) : v;
+            }
+
+
+            // Returns the path for a given data point.
+            function path(d) {
+                return vis.line(vis.dimensions.map(function (p) {
+                    return [position(p), vis.y[p](d[p])];
+                }));
+            }
         }
-        vis.dimensions.forEach(d => {
-            vis.y[d].brush = d3.brushY()
-                .extent([[-8, vis.y[d].range()[1]], [8, vis.y[d].range()[0]]])
-                .on('brush', vis.brush);
-        });
-
-        vis.x.domain(vis.dimensions);
-        // Extract the list of dimensions and create a scale for each.
 
 
-        vis.updateVis();
     }
-
-    updateVis() {
-        let vis = this;
-        // Add grey background lines for context.
-        vis.background = vis.svg.append("g")
-            .attr("class", "background")
-            .selectAll("path")
-            .data(vis.data)
-            .enter().append("path")
-            .attr("d", path);
-
-        // Add blue foreground lines for focus.
-        vis.foreground = vis.svg.append("g")
-            .attr("class", "foreground")
-            .selectAll("path")
-            .data(vis.data)
-            .enter().append("path")
-            .attr("d", path);
-
-        //// ==== Draw Axis START ==== ////
-        vis.g = vis.svg.selectAll(".dimension")
-            .data(vis.dimensions).enter() // g's data is the dimensions
-            .append("g")
-            .attr("class", "dimension")
-            // Give the axis it's propper x pos
-            .attr("transform", function (d) {
-                return "translate(" + vis.x(d) + ")";
-            });
-        vis.g.append("g") // The Axis
-            .attr("class", "axis")
-            .each(function (d) {
-                d3.select(this).call(vis.axis.scale(vis.y[d]));
-            });
-        vis.g.append("text") // Axis Label
-            .attr("class", "title")
-            .style("text-anchor", "middle")
-            .attr("y", -10)
-            .attr("font-size", 15)
-            .text(function (d) {
-                return d;
-            });
-        //// ==== Draw Axis END ==== ////
-
-        vis.g.append('g')
-            .attr('class', 'brush')
-            .each(function (d) {
-                d3.select(this).call(vis.y[d].brush);
-            })
-            .selectAll('rect')
-            .attr('x', -8)
-            .attr('width', 16);
-
-        vis.svg.selectAll(".brush").selectAll(".overlay")
-            .on("mousedown", vis.brush);
-
-
-
-        function position(d) {
-            var v = vis.dragging[d];
-            return v == null ? vis.x(d) : v;
-        }
-
-
-    // Returns the path for a given data point.
-        function path(d) {
-            return vis.line(vis.dimensions.map(function (p) {
-                return [position(p), vis.y[p](d[p])];
-            }));
-        }
-    }
-
-
-
-
-}
